f543b9348d2e5b502f11aea469e0be060a9deb62
serenejiang <Serene@Serenes-MacBook-Pro.local>
2017-01-06 12:12:07 -0800

    initial commit

---
+++ dfdr/DSfdr.py
@@ -0,0 +1,218 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+import scipy.stats
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# data transformation
+def rankdata(data):
+	rdata=np.zeros(np.shape(data))
+	for crow in range(np.shape(data)[0]):
+		rdata[crow,:]=sp.stats.rankdata(data[crow,:])
+	return rdata
+
+def logdata(data):
+	data[data < 2] = 2
+	data = np.log2(data)
+	return data
+
+def apdata(data):
+	data[data != 0] = 1
+	return data
+
+def normdata(data):
+	data = data / np.sum(data, axis = 0)
+	return data	
+
+
+# different methods to calculate test statistic
+def meandiff(data, labels):
+	mean0 = np.mean(data[:, labels==0], axis = 1)
+	mean1 = np.mean(data[:, labels==1], axis = 1)
+	tstat = abs(mean1 - mean0)
+	return tstat	
+
+
+def mannwhitney(data, labels):
+	group0 = data[:, labels == 0]
+	group1 = data[:, labels == 1]
+	tstat = np.array([scipy.stats.mannwhitneyu(group0[i, :], 
+		group1[i, :]).statistic for i in range(np.shape(data)[0])])
+	return tstat    
+
+
+# kruwallis give a column vector while others give row vector
+def kruwallis(data, labels):  
+	n = len(np.unique(labels))
+	allt=[]
+	for cbact in range(np.shape(data)[0]):
+		group = []
+		for j in range(n):
+			group.append(data[cbact, labels == j])
+		tstat = scipy.stats.kruskal(*group).statistic
+		allt.append(tstat)
+	return allt
+
+def stdmeandiff(data, labels):	
+	mean0 = np.mean(data[:, labels==0], axis = 1)
+	mean1 = np.mean(data[:, labels==1], axis = 1)
+	sd0 = np.std(data[:, labels==0], axis = 1, ddof = 1)
+	sd1 = np.std(data[:, labels==1], axis = 1, ddof = 1)
+	tstat = abs(mean1 - mean0)/(sd1 + sd0) 
+	return tstat
+		
+
+
+# new fdr method
+def pfdr(data,labels, method, transform=None, alpha=0.1,numperm=1000, fdrbefast=False):
+	data=data.copy()
+
+	if transform == 'rankdata':
+		data = rankdata(data)
+	elif transform == 'logdata':
+		data = logdata(data)
+	elif transform == 'apdata':
+		data = apdata(data) 
+	elif transform == 'normdata':
+		data = normdata(data) 	 
+
+	#print('permuting')
+	numbact=np.shape(data)[0]
+
+	if method == "meandiff":    
+		method = meandiff   
+		t=method(data,labels)
+		numsamples=np.shape(data)[1]
+		p=np.zeros([numsamples,numperm])
+		k1=1/np.sum(labels == 0)
+		k2=1/np.sum(labels == 1)
+		for cperm in range(numperm):
+			np.random.shuffle(labels)
+			p[labels==0, cperm] = k1
+		p2 = np.ones(p.shape)*k2
+		p2[p>0] = 0
+		mean1 = np.dot(data, p)
+		mean2 = np.dot(data, p2)
+		u = np.abs(mean1 - mean2)
+		
+	elif method == 'mannwhitney':
+		method = mannwhitney
+		t=method(data,labels)
+		u=np.zeros([numbact,numperm])
+		for cperm in range(numperm):
+			rlabels=np.random.permutation(labels)
+			rt=method(data,rlabels)
+			u[:,cperm]=rt
+
+	elif method == 'kruwallis':
+		method = kruwallis
+		t=method(data,labels)
+		u=np.zeros([numbact,numperm])
+		for cperm in range(numperm):
+			rlabels=np.random.permutation(labels)
+			rt=method(data,rlabels)
+			u[:,cperm]=rt	
+
+	elif method == 'stdmeandiff':
+		method = stdmeandiff
+		t=method(data,labels)
+		u=np.zeros([numbact,numperm])
+		for cperm in range(numperm):
+			rlabels=np.random.permutation(labels)
+			rt=method(data,rlabels)
+			u[:,cperm]=rt
+
+
+	elif method == 'spearman' or method == 'pearson':
+		if method == 'spearman':
+			data = rankdata(data)
+			labels = sp.stats.rankdata(labels)
+		meanval=np.mean(data,axis=1).reshape([data.shape[0],1])
+		data=data-np.repeat(meanval,data.shape[1],axis=1)
+		labels=labels-np.mean(labels)
+		t=np.abs(np.dot(data, labels))
+		permlabels = np.zeros([len(labels), numperm])
+		for cperm in range(numperm):
+			rlabels=np.random.permutation(labels)
+			permlabels[:,cperm] = rlabels
+		u=np.abs(np.dot(data,permlabels))	
+
+	elif method == 'nonzerospearman' or method == 'nonzeropearson':
+		t = np.zeros([numbact])
+		u = np.zeros([numbact, numperm])
+		for i in range(numbact):
+			index = np.nonzero(data[i, :])
+			label_nonzero = labels[index]
+			sample_nonzero = data[i, :][index]
+			if method == 'nonzerospearman':
+				sample_nonzero = sp.stats.rankdata(sample_nonzero)
+				label_nonzero = sp.stats.rankdata(label_nonzero)
+			sample_nonzero = sample_nonzero - np.mean(sample_nonzero)
+			label_nonzero = label_nonzero - np.mean(label_nonzero)
+			t[i] = np.abs(np.dot(sample_nonzero, label_nonzero))
+
+			permlabels = np.zeros([len(label_nonzero), numperm])
+			for cperm in range(numperm):
+				rlabels=np.random.permutation(label_nonzero)
+				permlabels[:,cperm] = rlabels
+			u[i, :] = np.abs(np.dot(sample_nonzero, permlabels))
+
+	else:
+		# method = userfunction 
+		#t=method(data,labels)         
+		u=np.zeros([numbact,numperm])
+		for cperm in range(numperm):
+			rlabels=np.random.permutation(labels)
+			rt=method(data,rlabels)
+			u[:,cperm]=rt
+
+	#print('calculating fdr')
+	#print(np.shape(u))
+
+	#print('fixing floating point errors')
+	for crow in range(numbact):
+		closepos=np.isclose(t[crow],u[crow,:])
+		u[crow,closepos]=t[crow]
+
+	# calculate permutation p-vals
+	for crow in range(numbact):
+		cvec=np.hstack([t[crow],u[crow,:]])
+		cvec=1-(sp.stats.rankdata(cvec,method='min')/len(cvec))
+		t[crow]=cvec[0]
+		u[crow,:]=cvec[1:]
+
+	# calculate FDR
+
+	# sort the p-values from big to small
+	sortt=list(set(t))
+	sortt=np.sort(sortt)
+	sortt=sortt[::-1]
+
+	foundit=False
+	allfdr=[]
+	allt=[]
+	#print(sortt)
+	for cp in sortt:
+		realnum=np.sum(t<=cp)
+		fdr=(realnum+np.count_nonzero(u<=cp)) / (realnum*(numperm+1))
+		allfdr.append(fdr)
+		allt.append(cp)
+		# print(fdr)
+		if fdr<=alpha:
+			realcp=cp
+			foundit=True
+			break
+
+	if not foundit:
+		#print('not low enough. number of rejects : 0')
+		reject=np.zeros(numbact,dtype=int)
+		reject= (reject>10) # just want to output "FALSE"
+		return reject
+
+	# and fill the reject null hypothesis
+	reject=np.zeros(numbact,dtype=int)
+	reject= (t<=realcp)
+	#print('rejected %d ' % (np.sum(t<=realcp)))
+	return reject
---
+++ dfdr/simulate.py
@@ -0,0 +1,274 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+import scipy.stats
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+# simulate data with two groups
+def simulatemix(numsamples=5,numdiff=100,numc=100,numd=800,noise=0,numreads=10000):
+	"""
+	create a simulation of 2 multinomial distributions
+
+	input:
+	numsamples : int
+		number of samples in each group
+	numdiff : int
+		number of different bacteria between the groups
+	numc : int
+		number of high freq. bacteria similar between the groups
+	numd : int
+		number of low freq. bacteria similar between the groups
+	noise : float
+		the noise
+	"""
+
+	# init otus different between A and B
+	# (same sum so no compositionallity effect)
+	Aa = np.random.uniform(0.1, 1, int(numdiff/2))
+	Ba = np.random.uniform(0.1, 1, int(numdiff/2))
+	#Ab = Ba
+	#Bb = Aa
+	# high freq. similar
+	Ac = Bc = np.random.uniform(0.1, 1, numc)
+	# low freq. similar
+	#Ad = Bd = np.random.uniform(0, 0.001, numd)
+	numnoise = np.random.randint(1, 7, numd)
+
+	# normalize the probabilities
+	#pA = np.hstack((Aa, Ab, Ac, Ad))
+	#pA = np.hstack((Aa, Ab, Ac))
+	pA = np.hstack((Aa, Ac))
+	pA_normed = pA / np.sum(pA)
+	#pB = np.hstack((Ba, Bb, Bc, Bd))
+	#pB = np.hstack((Ba, Bb, Bc))
+	pB = np.hstack((Ba, Bc))
+	pB_normed = pB / np.sum(pB)
+	#print(pA_normed)
+	# simulate data
+	numbact = np.shape(pA)[0]
+	A = np.zeros([numbact, numsamples])
+	B = np.zeros([numbact, numsamples])
+	for i in range(numsamples):
+		cpA=pA_normed+(np.random.randn(numbact)*noise*np.mean(pA_normed))
+		cpA[cpA<0]=0
+		cpA=cpA/np.sum(cpA)
+		cpB=pB_normed+(np.random.randn(numbact)*noise*np.mean(pA_normed))
+		cpB[cpB<0]=0
+		cpB=cpB/np.sum(cpB)
+		# rA = np.random.multinomial(numreads, pA_normed, size = 1)
+		# rB = np.random.multinomial(numreads, pB_normed, size = 1)
+		rA = np.random.multinomial(numreads, cpA, size = 1)
+		rB = np.random.multinomial(numreads, cpB, size = 1)
+		A[:, i] = rA
+		B[:, i] = rB
+	data = np.hstack((A, B))
+
+	D = np.zeros([numd, 2*numsamples])
+	for j in range(numd):
+		for cnoise in range(numnoise[j]):
+			cpos=np.random.randint(2*numsamples)
+			D[j,cpos]=1
+
+	data = np.vstack((data, D))
+
+	# labels
+	x = np.array([0, 1])
+	labels = np.repeat(x, numsamples)
+	labels = (labels==1)
+
+	return (data,labels)
+
+def simulatemix2(numsamples=5,numdiff=100,numc=100,numd=800,sigma=0.1,numreads=10000):
+	"""
+	revised simulation code
+
+	input:
+	numsamples : int
+		number of samples in each group
+	numdiff : int
+		number of different bacteria between the groups
+	numc : int
+		number of high freq. bacteria similar between the groups
+	numd : int
+		number of low freq. bacteria similar between the groups
+	sigma : float
+		the standard deviation
+	"""
+
+	A = np.zeros([int(numdiff), 2*numsamples])
+	#B = np.zeros([int(numdiff/2), 2*numsamples])
+	for i in range(int(numdiff)):
+		mu_H = np.random.uniform(0.1,1)
+		mu_S = np.random.uniform(0.1,1)
+		h = np.random.normal(mu_H, sigma, numsamples)
+		s = np.random.normal(mu_S, sigma, numsamples)
+		# zero inflation
+		h[h<0]=0
+		s[s<0]=0
+		A[i,:] = np.hstack((h,s))
+		#B[i,:] = np.hstack((s,h))
+
+	C = np.zeros([numc, 2*numsamples])
+	for j in range(numc):	
+		mu = np.random.uniform(0.1,1)
+		C[j,:] = np.random.normal(mu, sigma, 2*numsamples)
+
+	numnoise = np.random.randint(1, 7, numd)
+	D = np.zeros([numd, 2*numsamples])
+	for k in range(numd):
+		for cnoise in range(numnoise[k]):
+			cpos=np.random.randint(2*numsamples)
+			D[k,cpos]=np.random.uniform(0.1,1)
+
+	#data = np.vstack((A,B,C,D))
+	data = np.vstack((A,C,D))
+	data = data/np.sum(data, axis = 0) # normalize by column
+
+	# labels
+	x = np.array([0, 1])
+	labels = np.repeat(x, numsamples)
+	labels = (labels==1)
+
+	return (data,labels)
+
+
+
+# simulate data with more than 2 groups
+def multisimulatemix(numsamples=5,numgroup=3, numdiff=100,numc=100,numd=800,noise=0,numreads=10000):
+	"""
+	create a simulation of 2 multinomial distributions
+
+	input:
+	numsamples : int
+		number of samples in each group
+	numdiff : int
+		number of different bacteria between the groups
+	numc : int
+		number of high freq. bacteria similar between the groups
+	numd : int
+		number of low freq. bacteria similar between the groups
+	noise : float
+		the noise
+	"""
+
+	# init otus different between A and B
+	# (same sum so no compositionallity effect)
+	Aa = np.random.uniform(0.1, 1, int(numdiff/2))
+	Ba = np.random.uniform(0.1, 1, int(numdiff/2))
+	Ab = Ba
+	Bb = Aa
+	# high freq. similar
+	Ac = Bc = np.random.uniform(0.1, 1, numc)
+	# low freq. similar
+	#Ad = Bd = np.random.uniform(0, 0.001, numd)
+	numnoise = np.random.randint(1, 7, numd)
+
+	# normalize the probabilities
+	#pA = np.hstack((Aa, Ab, Ac, Ad))
+	pA = np.hstack((Aa, Ab, Ac))
+	pA_normed = pA / np.sum(pA)
+	#pB = np.hstack((Ba, Bb, Bc, Bd))
+	pB = np.hstack((Ba, Bb, Bc))
+	pB_normed = pB / np.sum(pB)
+
+	# simulate data
+	numbact = np.shape(pA)[0]
+	A = np.zeros([numbact, numsamples])
+	B = np.zeros([numbact, numsamples])
+	for i in range(numsamples):
+		cpA=pA_normed+(np.random.randn(numbact)*noise)
+		cpA[cpA<0]=0
+		cpA=cpA/np.sum(cpA)
+		cpB=pB_normed+(np.random.randn(numbact)*noise)
+		cpB[cpB<0]=0
+		cpB=cpB/np.sum(cpB)
+		# rA = np.random.multinomial(numreads, pA_normed, size = 1)
+		# rB = np.random.multinomial(numreads, pB_normed, size = 1)
+		rA = np.random.multinomial(numreads, cpA, size = 1)
+		rB = np.random.multinomial(numreads, cpB, size = 1)
+		A[:, i] = rA
+		B[:, i] = rB
+	data = np.hstack((A, B))
+
+	D = np.zeros([numd, 2*numsamples])
+	for j in range(numd):
+		for cnoise in range(numnoise[j]):
+			cpos=np.random.randint(2*numsamples)
+			D[j,cpos]=1
+	data = np.vstack((data, D))
+
+	for i in range(numgroup-2):
+		data = np.hstack((data, data[:, 0:numsamples]))
+
+	# labels 
+	x = np.arange(numgroup)
+	labels = np.repeat(x, numsamples)
+	for j in range(numgroup):
+		labels == j
+	return (data,labels)
+
+# simulate data for correlation test
+def simulatecor(numa=100,numc=100,numd=800, numsamples = 10):
+	"""
+	create a simulation of 2 multinomial distributions
+
+	input:
+	numa : int
+		number of bacterials correlated
+	numb : int
+		balance the compositionaly with section A
+	numc : int
+		number of bacterial uncorrelated
+	numd : int
+		number of low freq. bacteria similar between the groups
+	numsamples : int
+		number of samples for each bacteria
+	"""
+
+	labels = np.arange(1, int(numsamples + 1))
+	A = np.zeros([numa, numsamples])
+	for i in range(int(numa)):
+		u = np.random.uniform(-1,1)
+#		u=-1
+		s = np.random.uniform(0, 100)
+		#r = np.random.uniform(-2, 2) Problem: negative power produces inf
+		#r = np.random.uniform(0, 2)
+		r = np.random.uniform(1, 2)
+#		r=1
+#		s=100
+		coin=np.random.randint(2)
+		if coin == 0:
+			a = s + u*((labels)**r)
+		else:
+#			a = s + u*((labels)**r)
+			a = s + u*((numsamples+1 - labels)**r)
+		A[i,:] = a
+
+	T = np.sum(A, axis = 0) # column sum
+	B = max(T) - T
+	#print(np.shape(B))
+
+
+	numc = 100 # how to choose C
+	C = np.zeros([numc, numsamples])
+	for i in range(numc):
+		c = np.random.uniform(3000, 3600, numsamples)
+		C[i, :] = c
+	#print(np.shape(C))  
+
+	numd = 100
+	numnoise = np.random.randint(1, 7, numd)
+	D = np.zeros([numd, numsamples])
+	for j in range(numd):
+		for cnoise in range(numnoise[j]):
+			cpos=np.random.randint(numsamples)
+			D[j,cpos]=1
+	#print(np.shape(D)) 
+
+	data = np.vstack((A, B, C, D))
+	#print(np.shape(data))
+	#print(data[0,:])
+	return (data,labels)
