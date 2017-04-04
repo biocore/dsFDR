caa1a2c9249dad54a2e2e55bb77c0d95b5426283
Serene Jiang <serene1030@gmail.com>
2017-04-03 17:16:19 -0700

    speed up statistics calculation

--- dsfdr/statistics.py
+++ dsfdr/statistics.py
@@ -1,58 +1,149 @@
 import numpy as np
-import pandas as pd
-import scipy as sp
 import scipy.stats
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+from scipy.stats import rankdata
 
 
 # different methods to calculate test statistic
 def meandiff(data, labels):
-	mean0 = np.mean(data[:, labels==0], axis = 1)
-	mean1 = np.mean(data[:, labels==1], axis = 1)
-	tstat = mean1 - mean0
-	return tstat	
+    mean0 = np.mean(data[:, labels == 0], axis=1)
+    mean1 = np.mean(data[:, labels == 1], axis=1)
+    tstat = mean1 - mean0
+    return tstat
 
 
-def stdmeandiff(data, labels):	
-	mean0 = np.mean(data[:, labels==0], axis = 1)
-	mean1 = np.mean(data[:, labels==1], axis = 1)
-	sd0 = np.std(data[:, labels==0], axis = 1, ddof = 1)
-	sd1 = np.std(data[:, labels==1], axis = 1, ddof = 1)
-	tstat = (mean1 - mean0)/(sd1 + sd0) 
-	return tstat
+def stdmeandiff(data, labels):
+    mean0 = np.mean(data[:, labels == 0], axis=1)
+    mean1 = np.mean(data[:, labels == 1], axis=1)
+    sd0 = np.std(data[:, labels == 0], axis=1, ddof=1)
+    sd1 = np.std(data[:, labels == 1], axis=1, ddof=1)
+    tstat = (mean1 - mean0) / (sd1 + sd0)
+    return tstat
+
+
+# calculate test statistic for Mann Whitney
+def mannwhitneyU(x, y):
+    n1 = len(x)
+    n2 = len(y)
+    ranked = rankdata(np.concatenate((x, y)))
+    # default: average for ties
+    rankx = ranked[0:n1]
+    Ux = n1 * n2 + (n1 * (n1 + 1)) / 2.0 - np.sum(rankx, axis=0)
+    Uy = n1 * n2 - Ux
+    U = min(Ux, Uy)
+    return U
 
 
 def mannwhitney(data, labels):
-	group0 = data[:, labels == 0]
-	group1 = data[:, labels == 1]
-	tstat = np.array([scipy.stats.mannwhitneyu(group0[i, :], 
-		group1[i, :]).statistic for i in range(np.shape(data)[0])])
-	return tstat    
+    group0 = data[:, labels == 0]
+    group1 = data[:, labels == 1]
+    tstat = np.array([mannwhitneyU(group0[i, :], group1[i, :])
+                     for i in range(np.shape(data)[0])])
+    return tstat
 
-def kruwallis(data, labels):  
-	n = len(np.unique(labels))
-	allt=[]
-	for cbact in range(np.shape(data)[0]):
-		group = []
-		for j in range(n):
-			group.append(data[cbact, labels == j])
-		tstat = scipy.stats.kruskal(*group).statistic
-		allt.append(tstat)
-	return allt
 
-def pearson(data, labels):
-	tstat = np.array([scipy.stats.pearsonr(data[i, :], 
-		labels)[0] for i in range(np.shape(data)[0])])
-	return tstat  
+# calculate test statistic for Kruskal-Wallis
+def tiecorrect(rankvals):
+    arr = np.sort(rankvals)
+    idx = np.nonzero(np.r_[True, arr[1:] != arr[:-1], True])[0]
+    cnt = np.diff(idx).astype(np.float64)
+
+    size = np.float64(arr.size)
+    return 1.0 if size < 2 else 1.0 - (cnt**3 - cnt).sum() / (size**3 - size)
+
+
+def _chk_asarray(a, axis):
+    if axis is None:
+        a = np.ravel(a)
+        outaxis = 0
+    else:
+        a = np.asarray(a)
+        outaxis = axis
+
+    if a.ndim == 0:
+        a = np.atleast_1d(a)
+
+    return a, outaxis
+
+
+def _square_of_sums(a, axis=0):
+    a, axis = _chk_asarray(a, axis)
+    s = np.sum(a, axis)
+    if not np.isscalar(s):
+        return s.astype(float) * s
+    else:
+        return float(s) * s
+
+
+def kruskalH(args):
+    num_groups = len(args)
+    if num_groups < 2:
+        raise ValueError("Need at least two groups in stats.kruskal()")
+    n = np.asarray(list(map(len, args)))  # samples in each group
+    alldata = np.concatenate(args)
+    ranked = rankdata(alldata)
+    ties = tiecorrect(ranked)
+    if ties == 0:
+        raise ValueError('All numbers are identical in kruskal')
+    # Compute sum^2/n for each group and sum
+    j = np.insert(np.cumsum(n), 0, 0)
+    ssbn = 0
+    for i in range(num_groups):
+        ssbn += _square_of_sums(ranked[j[i]:j[i + 1]]) / float(n[i])
+
+    totaln = np.sum(n)
+    h = 12.0 / (totaln * (totaln + 1)) * ssbn - 3 * (totaln + 1)
+    h /= ties
+    return h
+
+
+def kruwallis(data, labels):
+    n = len(np.unique(labels))
+    allt = []
+    for cbact in range(np.shape(data)[0]):
+        group = []
+        for j in range(n):
+            group.append(data[cbact, labels == j])
+        tstat = kruskalH(group)
+        allt.append(tstat)
+    return allt
+
+
+# pearson correlation
+def _sum_of_squares(a, axis=0):
+    a, axis = _chk_asarray(a, axis)
+    return np.sum(a * a, axis)
+
+
+def pearsonR(x, y):
+    mx = x.mean()
+    my = y.mean()
+    xm, ym = x - mx, y - my
+    r_num = np.add.reduce(xm * ym)
+    r_den = np.sqrt(_sum_of_squares(xm) * _sum_of_squares(ym))
+    r = r_num / r_den
+    return r
+
+
+# spearman correlation
+def spearmanR(a, b=None, axis=0):
+    a, axisout = _chk_asarray(a, axis)
+    ar = np.apply_along_axis(rankdata, axisout, a)
+
+    br = None
+    if b is not None:
+        b, axisout = _chk_asarray(b, axis)
+        br = np.apply_along_axis(rankdata, axisout, b)
+    rs = np.corrcoef(ar, br, rowvar=axisout)
+    if rs.shape == (2, 2):
+        return rs[1, 0]
+    else:
+        return rs
 
 
 def spearman(data, labels):
-	tstat = np.array([scipy.stats.spearmanr(data[i, :], 
-		labels).correlation for i in range(np.shape(data)[0])])
-	return tstat  				
-
+    tstat = np.array([spearmanR(data[i, :], labels)
+                      for i in range(np.shape(data)[0])])
+    return tstat
 
 
 
--- dsfdr/transform.py
+++ dsfdr/transform.py
@@ -1,41 +1,35 @@
 import numpy as np
-import pandas as pd
-import scipy as sp
 import scipy.stats
 from skbio.stats.composition import clr
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 # data transformation
 def rankdata(data):
-	rdata = np.zeros(np.shape(data))
-	for crow in range(np.shape(data)[0]):
-		rdata[crow,:] = sp.stats.rankdata(data[crow,:])
-	return rdata
+    rdata = np.zeros(np.shape(data))
+    for crow in range(np.shape(data)[0]):
+        rdata[crow, :] = scipy.stats.rankdata(data[crow, :])
+    return rdata
+
 
 def log2data(data):
-	data[data < 2] = 2
-	data = np.log2(data)
-	return data
+    data[data < 2] = 2
+    data = np.log2(data)
+    return data
+
 
 def binarydata(data):
-	data[data != 0] = 1
-	return data
+    data[data != 0] = 1
+    return data
+
 
 def normdata(data):
-	data = data / np.sum(data, axis = 0)
-	return data	
+    data = data / np.sum(data, axis=0)
+    return data
+
 
 def clrdata(data):
-	data[data == 0] = 1
-	clrdata = np.zeros(np.shape(data))
-	for ncol in range(np.shape(data)[1]):
-		clrdata[:, ncol] = clr(data[:, ncol])
-	return clrdata	
-
-#def clrdata(data):
-#	clrdata = np.zeros(np.shape(data))
- #   for ncol in range(np.shape(data)[1]):
-  #  	clrdata[:, ncol] = clr(data[:, ncol])
-   # return clrdata
+    data[data == 0] = 1
+    clrdata = np.zeros(np.shape(data))
+    for ncol in range(np.shape(data)[1]):
+        clrdata[:, ncol] = clr(data[:, ncol])
+    return clrdata
